e440bc073cc4323bf392ba39ee339b9f353f6dbf
Ian King <ian.antking@gmail.com>
2018-09-24 20:22:47 +0100

    added player and movement

--- js/main.js
+++ js/main.js
@@ -9,15 +9,36 @@ let config = {
 
 let game = new Phaser.Game(config);
 
+gameScene.init = function init(){
+    this.playerSpeed = 1.5;
+    this.enemyMaxY = 280;
+    this.enemyMinY = 80;
+};
+
 gameScene.preload = function preload(){
     this.load.image('background', 'assets/background.png');
+    this.load.image('player', 'assets/player.png');
+    this.load.image('dragon', 'assets/dragon.png');
+    this.load.image('treasure', 'assets/treasure.png');
 };
 
 gameScene.create = function create(){
+
+    const centerY = (this.sys.game.config.height / 2);
+
     this.background = this.add.sprite(0,0, 'background');
     this.background.setOrigin(0);
+
+    this.player = this.add.sprite(40, centerY, 'player');
+    this.player.setScale(0.5);
+
+
 };
 
 gameScene.update = function update(){
+
+    if (this.input.activePointer.isDown){
+        this.player.x += this.playerSpeed;
+    };
 
 };
